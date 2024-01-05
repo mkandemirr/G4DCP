6d351ea0fd77b2c9165eacb558ee003afd442fd5
Mustafa Kandemir <114905224+mkandemirr@users.noreply.github.com>
2024-01-05 23:22:01 +0300

    Update VComponentBuilder.hh

--- G4DCP/VComponentBuilder.hh
+++ G4DCP/VComponentBuilder.hh
@@ -7,7 +7,6 @@
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 template <typename T>
 class VComponentBuilder{
-
   public:
     VComponentBuilder();
     virtual ~VComponentBuilder(){};
@@ -16,9 +15,7 @@ class VComponentBuilder{
   
   protected:
     virtual T* Build() = 0;   
- 	
- 		T* product;
-      
+    T* product;      
 };
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -31,10 +28,10 @@ VComponentBuilder<T>::VComponentBuilder():product{nullptr}
 template <typename T>
 T* VComponentBuilder<T>::GetProduct()
 {
-	if(product)
-		return product;
-	else	
-  	return Build();
+  if(product)
+    return product;
+  else
+    return Build();
 }
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
